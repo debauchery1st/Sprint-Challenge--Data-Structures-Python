5c40b1a54db625554c61d6b12d58cbda460ec966
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-15 15:56:18 -0400

    pythonic solutions

--- names/names.py
+++ names/names.py
@@ -1,5 +1,7 @@
 import time
 from os import path
+from ls_bst import BinarySearchTree
+
 src_dir = path.split(path.abspath(__file__))[0]  # absolute path to source file
 names1 = path.join(src_dir, 'names_1.txt')  # absoloute path to names_1.txt
 names2 = path.join(src_dir, 'names_2.txt')  # absolute path to names_2.txt
@@ -15,11 +17,21 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# original nested for_loop => runtime: 5.117471933364868 seconds
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# solution 1: one for-loop =>  runtime: 1.2671453952789307 seconds
+for x in names_1:
+    if x in names_2:
+        duplicates.append(x)
+
+
+# solution 2: list comprehension => runtime: 1.7966792583465576 seconds
+[duplicates.append(i) for i in names_1 if i in names_2]
+
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
