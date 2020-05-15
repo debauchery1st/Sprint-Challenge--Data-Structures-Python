f7d32d5b441612a234cd14054dd5884c12e8c495
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-15 14:36:00 -0400

    use absolute paths

--- names/names.py
+++ names/names.py
@@ -1,12 +1,15 @@
 import time
-
+from os import path
+src_dir = path.split(path.abspath(__file__))[0]  # absolute path to source file
+names1 = path.join(src_dir, 'names_1.txt')  # absoloute path to names_1.txt
+names2 = path.join(src_dir, 'names_2.txt')  # absolute path to names_2.txt
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open(names1, 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open(names2, 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
@@ -19,8 +22,8 @@ for name_1 in names_1:
             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
